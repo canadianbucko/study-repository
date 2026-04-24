687f0a37b26313c0a5735b709dea74a0bdeb1015
dumbturtle13 <kirikqaz1234@gmail.com>
2026-04-24 18:51:39 +0300

    Update simple_connection.go

--- databasesPt_1/simple_sql/simple_connection.go
+++ databasesPt_1/simple_sql/simple_connection.go
@@ -10,7 +10,7 @@ import (
 // "postgres://YourUserName:YourPassword@YourHostName:5432/YourDatabaseName"
 
 func CreateConnection(ctx context.Context) (*pgx.Conn, error) {
-	connectionString := os.Getenv("CONN_STRING")
+	connectionString := os.Getenv("CONN_STRING") // не забудь экспортировать в мейкфайле если так include .env + export!
 
 	conn, err := pgx.Connect(ctx, connectionString)
 
